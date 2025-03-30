f36b11cb06f0a1d87118f01c9802b557455a8db2
matintohidi <matintovhidi@gmail.com>
2025-03-30 16:42:07 +0330

    Add voice recording functionality to the chat component and improve UI interactions

--- src/app/(chat)/chat/_components/message/sendMessage.tsx
+++ src/app/(chat)/chat/_components/message/sendMessage.tsx
@@ -1,14 +1,33 @@
+"use client";
+
 import React from "react";
 import { Microphone, Paperclip, Send2 } from "iconsax-react";
+import dynamic from "next/dynamic";
+
+const VoiceRecorder = dynamic(
+  () => import("@/app/(chat)/chat/_components/message/voiceRecorder"),
+  {
+    ssr: false,
+    loading: () => (
+      <button
+        className="rounded-lg w-10 h-10 flex items-center justify-center animate-pulse disabled:bg-gray-200"
+        disabled
+      >
+        <Microphone size={24} color="#383a47" />
+      </button>
+    ),
+  }
+);
 
 const SendMessage: React.FC = () => {
+  console.log("render");
+
   return (
     <div className="flex p-6 gap-4 items-center h-36">
       <div className="flex flex-col justify-between h-full">
-        <button className="bg-secondary rounded-lg w-10 h-10 flex items-center justify-center">
-          <Microphone size={24} color="#615EF0" />
-        </button>
-        <button className="bg-secondary rounded-lg w-10 h-10 flex items-center justify-center">
+        <VoiceRecorder />
+
+        <button className="bg-secondary rounded-lg w-10 h-10 flex items-center justify-center transition duration-125 focus:outline-none focus:ring-1 focus:ring-primary focus:ring-offset-2 hover:bg-primary/15">
           <Paperclip size={24} color="#615EF0" />
         </button>
       </div>
@@ -18,7 +37,7 @@ const SendMessage: React.FC = () => {
           className="pl-5 pr-14 py-2.5 outline-0 text-primaryText placeholder:text-sm placeholder-gray-400 border-gray-200 border-2 rounded-xl w-full scroll-smooth resize-none h-full"
           placeholder="Type a message"
         ></textarea>
-        <button className="absolute right-4 top-4 bg-secondary rounded-lg w-10 h-10 flex items-center justify-center transition duration-125 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 hover:bg-primary/15">
+        <button className="absolute right-4 top-4 bg-secondary rounded-lg w-10 h-10 flex items-center justify-center transition duration-125 focus:outline-none focus:ring-1 focus:ring-primary focus:ring-offset-2 hover:bg-primary/15">
           <Send2 variant="Bold" color="#615EF0" size={24} />
         </button>
       </div>
---
+++ src/app/(chat)/chat/_components/message/voiceRecorder.tsx
@@ -0,0 +1,31 @@
+import { Microphone, MicrophoneSlash1, Trash } from "iconsax-react";
+import React from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
+
+const VoiceRecorder: React.FC = () => {
+  const { status, startRecording, stopRecording, clearBlobUrl, mediaBlobUrl } =
+    useReactMediaRecorder({ audio: true });
+  console.log("status", status, "mediaBlobUrl", mediaBlobUrl);
+  return (
+    <button
+      className="bg-secondary rounded-lg w-10 h-10 flex items-center justify-center transition duration-125 focus:outline-none focus:ring-1 focus:ring-primary focus:ring-offset-2 hover:bg-primary/15"
+      onClick={
+        status === "recording"
+          ? stopRecording
+          : status === "stopped"
+            ? clearBlobUrl
+            : startRecording
+      }
+    >
+      {status === "recording" ? (
+        <MicrophoneSlash1 size={24} color="#615ef0" />
+      ) : status === "stopped" ? (
+        <Trash size={24} color="#615ef0" />
+      ) : (
+        <Microphone size={24} color="#615ef0" />
+      )}
+    </button>
+  );
+};
+
+export default VoiceRecorder;
