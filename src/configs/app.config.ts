3906dc03632457e2eacfa7401102150ec846404e
matintohidi <matintovhidi@gmail.com>
2025-08-11 22:54:47 +0330

    wip

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,1 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+console.log(process.env.NEXT_PUBLIC_API_URL);
+console.log(API_URL);
